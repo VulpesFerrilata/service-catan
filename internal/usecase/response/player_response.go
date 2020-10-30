3a3f0e0bc58bdd0bf4f8355d7fdc0fbac456cf23
SillyJester <mrsillyjester@gmail.com>
2020-10-30 18:38:06 +0700

    implement catan service

--- internal/usecase/response/player_response.go
+++ internal/usecase/response/player_response.go
@@ -1,5 +1,16 @@
 package response
 
+import "github.com/VulpesFerrilata/catan/internal/domain/model"
+
+func NewPlayerResponse(player *model.Player) *PlayerResponse {
+	playerResponse := new(PlayerResponse)
+	playerResponse.ID = int(player.ID)
+	playerResponse.GameID = int(player.GameID)
+	playerResponse.Color = player.Color
+	playerResponse.TurnOrder = player.TurnOrder
+	return playerResponse
+}
+
 type PlayerResponse struct {
 	ID        int           `json:"id"`
 	GameID    int           `json:"gameId"`
